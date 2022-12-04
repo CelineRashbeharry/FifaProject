40775c68864ae6fa221b5990b6037da8be41b95c
CelineRashbeharry <celinerashbeharry@gmail.com>
2022-12-04 13:12:45 -0500

    My initial Commit

---
+++ src/com/qa/fifa/SearchTest.java
@@ -0,0 +1,41 @@
+package com.qa.fifa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchTest {
+
+	@Test
+	public void validateSearch() {
+
+		// Setup ChromeDriver
+		WebDriverManager.chromedriver().setup();
+		// Create a WebDriver object
+		WebDriver driver = new ChromeDriver();
+
+		// open the url
+		driver.get("https://www.google.com");
+		// maximize the window
+		driver.manage().window().maximize();
+		// type in search
+		driver.findElement(By.name("q")).sendKeys("fifa world cup 2022");
+		// click on search
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		// capture the WebElement
+		WebElement firstLink = driver.findElement(By.xpath("(//div[@class='ofy7ae'])[1]"));
+		String actualText = firstLink.getText();
+		String expected = "FIFA World Cup Qatar 2022™";
+		//adding assertion to the test
+		Assert.assertTrue(actualText.equalsIgnoreCase(expected),
+				"Expected" + expected + " but found >>" + actualText);
+
+	}
+
+}
